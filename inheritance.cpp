75a9260be7995e448969361d7e941f40838d7b51
Shubham Bodkhe <109900001+Shubham0724@users.noreply.github.com>
2022-10-16 07:53:37 -0700

    Add files via upload

---
+++ inheritance.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+// Base Class
+class Employee
+{
+public:
+    int id;
+    float salary;
+    Employee(int inpId)
+    {
+        id = inpId;
+        salary = 34.0;
+    }
+    Employee() {}
+};
+
+// Creating a Programmer class derived from Employee Base class
+class Programmer : public Employee
+{
+public:
+    int languageCode;
+    Programmer(int inpId)
+    {
+        id = inpId;
+        languageCode = 9;
+    }
+    void getData(){
+        cout<<id<<endl;
+    }
+};
+int main()
+{
+    Employee shubham(1), central(2);
+    cout << shubham.salary << endl;
+    cout << central.salary << endl;
+    Programmer skillF(10);
+    cout << skillF.languageCode<<endl;
+    cout << skillF.id<<endl;
+    skillF.getData();
+    return 0;
+}
